0a72f8f923f638b0c8157b79eae4abff13c75b20
Maksym Teplyy <maksym.teplyy@atlas-iac.com>
2020-02-27 12:18:49 +0200

    Add React Mobile template

---
+++ react-component/___ComponentName___/___ComponentName___.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import cx from 'classnames';
+
+const ___ComponentName___ = ({ className }) => {
+  const containerClassName = cx(className); 
+
+  return (
+    <div className={containerClassName}>
+      
+    </div>
+  );
+};
+
+export default ___ComponentName___;
